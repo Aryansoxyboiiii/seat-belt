6794a29f6d85098b71f459a1fd7a88928aab0007
castolGodal <aryanpuri0711@gmail.com>
2022-05-04 19:47:47 +0530

    final-verson

--- messege.py
+++ messege.py
@@ -1,9 +1,11 @@
-# import required module
+
 import requests
 import json
-import seatbelt
-'''refrence 
-https://www.geeksforgeeks.org/send-text-messages-to-any-mobile-number-using-fast2sms-api-in-python/?ref=rp'''
+import random
+
+
+Challannumber = random.randint(12345, 987654 ) 
+#https://www.geeksforgeeks.org/send-text-messages-to-any-mobile-number-using-fast2sms-api-in-python/
 
 # mention url
 url = "https://www.fast2sms.com/dev/bulk"
@@ -15,34 +17,31 @@ my_data = {
 	'sender_id': 'FSTSMS',
 	
 	# Put your message here!
-	'message': 'This is a test message',
+	'message': 'your challan number is 53636363683 pls pay on paytm or google pay or contact on 73231422',
 	
 	'language': 'english',
 	'route': 'p',
 	
-	# You can send sms to multiple numbers
-	# separated by comma.
+	
 	'numbers': '9540609984'	
 }
 
 # create a dictionary
 headers = {
-	'authorization': 'na',
+	'authorization': 'NYX6I7e4HGrmjPBnUTwOvb09FoCa5SsxuZtJz3WiMgEKLph8fd7OXMlxSQIpAb9r2atGNekzhcPURyBu',
 	'Content-Type': "application/x-www-form-urlencoded",
-	'Cache-Control': "no-cache"
+	'Cache-Control': "no-cache" 
 }
+	
 
-####NYX6I7e4HGrmjPBnUTwOvb09FoCa5SsxuZtJz3WiMgEKLph8fd7OXMlxSQIpAb9r2atGNekzhcPURyBu#####
+def Response():
+	requests.request("POST",
+							url,
+							data = my_data,
+							headers = headers)
 
-
-# make a post request
-response = requests.request("POST",
-                            url,
-                            data = my_data,
-                            headers = headers)
-#
 #load json data from source
-returned_msg = json.loads(response.text)
-  
+
+#returned_msg = json.loads(Response.text)
+
 # print the send message
-print(returned_msg['message'])
